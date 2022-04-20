c49b6d138013b353c56b42c26614e776d9b71f5f
Ben Vizy <benvizy@gmail.com>
2022-04-20 15:37:47 -0400

    Add changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "BenjiForms",
+    platforms: [.iOS(.v13),.macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BenjiForms",
+            type: .dynamic,
             targets: ["BenjiForms"]),
     ],
     dependencies: [
