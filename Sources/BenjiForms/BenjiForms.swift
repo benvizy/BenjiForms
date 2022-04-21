4286fd74fec7545b1b5bef637fee1ba3a7423770
Ben Vizy <benvizy@gmail.com>
2022-04-21 12:12:01 -0400

    Add new class and unit test

--- Sources/BenjiForms/BenjiForms.swift
+++ Sources/BenjiForms/BenjiForms.swift
@@ -1,6 +1,24 @@
-public struct BenjiForms {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct BenjiForms {
+    public struct MainTextField: View {
+        
+        @State var placeholder: String
+        @Binding var text: String
+        
+        public init(placeholder: String, text: Binding<String>) {
+            self._placeholder = State(initialValue: placeholder)
+            self._text = text
+        }
+        
+        public var body: some View {
+            HStack {
+                Image(systemName: "person").foregroundColor(Color.blue)
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .foregroundColor(Color.blue)
+            }.padding()
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
+        }
     }
 }
--- Tests/BenjiFormsTests/BenjiFormsTests.swift
+++ Tests/BenjiFormsTests/BenjiFormsTests.swift
@@ -1,11 +1,23 @@
 import XCTest
+import SwiftUI
 @testable import BenjiForms
 
 final class BenjiFormsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BenjiForms().text, "Hello, World!")
+    @State var text: String
+    
+    public init(text: String) {
+        self.text = text
+        super.init()
     }
+    
+    func testExample() {
+        var body: some View {
+            BenjiForms.MainTextField(placeholder: "Testing", text: $text)
+        }
+    }
+    
+    static var allTests = [
+        ("testExample", testExample)
+    ]
+    
 }
